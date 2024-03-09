bbcf994d9979e27c507e2ea470271c226dfc1203
AnitaPrzedwojewska <nisiek@wp.pl>
2024-03-09 14:49:35 +0100

    changed files structure

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import './js/tmdb-api';
+
+import {
+  fetchTrendingMovies,
+  fetchSearchMovies,
+  fetchMovieDetails,
+  fetchMovieTrailers,
+} from './js/tmdb-api';
+import { showGallery } from './js/gallery';
+
+const form = document.querySelector('#search-form');
+const search = form.querySelector('#search-input');
+const moviesGallery = document.querySelector('#movies-gallery');
+console.log('moviesGallery: ', moviesGallery);
+
+let searchWords;
+let keywords;
+let page;
+let pages;
+
+// send new words for new searching
+// form.addEventListener('submit', showMoviesGallery);
+
+showMovies();
+// showGenres();
+
+async function showMovies() {
+  try {
+    const page = 1;
+    const keywords = 'marvel';
+    const movieId = 693134;
+    // const moviesList = await fetchTrendingMovies(page);
+    // const moviesList = await fetchSearchMovies(keywords, page);
+    // const moviesList = await fetchMovieDetails(movieId);
+    const moviesList = await fetchMovieTrailers(movieId);
+    console.log('results: ', moviesList.results);
+    // showGallery(moviesList.results, moviesGallery);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// async function showGenres() {
+//   try {
+//     const genresList = await getGenres();
+//   } catch {
+//     console.log(error);
+//   }
+// }
